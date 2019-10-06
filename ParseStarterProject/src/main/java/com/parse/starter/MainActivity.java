a26e5531901555d2ebd0e866d7f50c0c17f61277
Turi-byte <breldanmuturi001@gmail.com>
2019-10-06 21:28:32 +0300

    Add Parse comfiguration for saving through parse

---
+++ ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright (c) 2015-present, Parse, LLC.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+package com.parse.starter;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Switch;
+
+import com.parse.FindCallback;
+import com.parse.GetCallback;
+import com.parse.LogInCallback;
+import com.parse.Parse;
+import com.parse.ParseAnalytics;
+import com.parse.ParseAnonymousUtils;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+import com.parse.SignUpCallback;
+
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+//    ParseUser user = new ParseUser();
+//
+//    user.setUsername("Turi");
+//
+//    user.setPassword("Sunrise@1997");
+//
+//    user.signUpInBackground(new SignUpCallback() {
+//      @Override
+//      public void done(ParseException e) {
+//
+//        if(e == null){
+////          Okay
+//          Log.i("Sign up Okay","WE did this!");
+//
+//        } else {
+//
+//          e.printStackTrace();
+//
+//        }
+//
+//      }
+//    });
+
+//    ParseUser.logInInBackground("Turi", "Sunrise@1997", new LogInCallback() {
+//      @Override
+//      public void done(ParseUser user, ParseException e) {
+//        if(user != null){
+//
+//          Log.i("success", "We logged in");
+//
+//        } else {
+//
+//          e.printStackTrace();
+//
+//        }
+//
+//      }
+//    });
+
+    ParseUser.logOut();
+
+    if(ParseUser.getCurrentUser() != null){
+
+      Log.i("Signed In", ParseUser.getCurrentUser().getUsername());
+
+    } else {
+
+      Log.i("no luck","Not signed in");
+
+      }
+    ParseAnalytics.trackAppOpenedInBackground(getIntent());
+  }
+
+}
---
+++ ParseStarterProject/src/main/java/com/parse/starter/StarterApplication.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2015-present, Parse, LLC.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+package com.parse.starter;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+
+public class StarterApplication extends Application {
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+
+    // 2lBPdpHNjkLF
+
+    // Enable Local Datastore.
+    Parse.enableLocalDatastore(this);
+
+    // Add your initialization code here
+    Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
+            .applicationId("d19fc5e1f7e395b5b3d2285dcf8ffa8f11a12e1d")
+            .clientKey("e5e3959a1bd65f399a646c251170a1c4d1be6b9d")
+            .server("http://3.19.209.50:80/parse/")
+            .build()
+    );
+
+
+
+//    ParseUser.enableAutomaticUser();
+
+    ParseACL defaultACL = new ParseACL();
+    defaultACL.setPublicReadAccess(true);
+    defaultACL.setPublicWriteAccess(true);
+    ParseACL.setDefaultACL(defaultACL, true);
+
+  }
+}
